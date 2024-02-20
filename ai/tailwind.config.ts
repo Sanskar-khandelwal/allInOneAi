eb88a57a9134a8bea0e913841170a15fa31acb67
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2024-02-20 20:50:43 +0530

    config

--- ai/tailwind.config.ts
+++ ai/tailwind.config.ts
@@ -53,6 +53,9 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      fontFamily: {
+        Poppins: ["var(--font-poppins)"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
