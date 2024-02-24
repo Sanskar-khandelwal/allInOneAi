d4f14d8a70bb3ea93127e9d0b23ae017e52c9121
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2024-02-24 23:15:42 +0530

    compoents added

--- ai/src/app/(root)/transformations/add/[type]/page.tsx
+++ ai/src/app/(root)/transformations/add/[type]/page.tsx
@@ -2,13 +2,24 @@ import Header from "@/components/shared/Header";
 import React from "react";
 import { transformationTypes } from "../../../../../../constants";
 import TransformationForm from "@/components/shared/TransformationForm";
+import { getUserById } from "../../../../../../lib/actions/user.actions";
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
   const transformation = transformationTypes[type];
+  const user = await getUserById("1234");
   return (
     <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
-      <TransformationForm />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   );
 };
