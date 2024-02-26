47bab1365afb7f5f2392afd3f0da0230ea5f2b81
Sanskar Khandelwal <sanskarkhandelwal336@gmail.com>
2024-02-26 23:33:00 +0530

    toast added

--- ai/src/app/(root)/layout.tsx
+++ ai/src/app/(root)/layout.tsx
@@ -1,17 +1,19 @@
 import MobileNav from "@/components/shared/MobileNav";
 import Sidebar from "@/components/shared/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="root">
-      <Sidebar />
-      <MobileNav />
-      <div className="root-container">
-        <div>{children}</div>
-      </div>
-    </main>
-  );
+ return (
+  <main className="root">
+   <Sidebar />
+   <MobileNav />
+   <div className="root-container">
+    <div>{children}</div>
+   </div>
+   <Toaster />
+  </main>
+ );
 };
 
 export default layout;
